e93dbf10bc6ecf220ef6a61bff9dfdfd5f1b8fbf
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-17 20:11:52 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -28,13 +28,3 @@ print (" ")
 ngrok = input ('введите цифру>>>')
 if ngrok == '1':    
     print ("1")
-fish = input ('введите цифру>>>')
-elif fish == '2'    
-    print ("2")
-Cam = input('введите цифру>>>')
-elif Cam == '5':    
-    print ("1")
-else:
-        print("Вы ввели не правильное значение"
-
-
