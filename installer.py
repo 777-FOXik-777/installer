bc1f5158e96143eba276e0723774d1fe60cfbc2a
Ratata <84903507+PY079@users.noreply.github.com>
2022-03-18 22:30:28 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -55,7 +55,7 @@ while True:
         os.system('./termux-ngrok.sh')
     
     if inp == '2':   
-        os.system('cd $HOME')
+        print(dir)
         os.system('cd PyPhisher')
         time.sleep(1)
         os.system('python3 pyphisher.py')
