b660d5c7ff56a1e12c855dc6c9dd079d464dbd88
Ratata <84903507+PY079@users.noreply.github.com>
2022-03-18 22:38:22 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -57,7 +57,6 @@ while True:
     if inp == '2':   
         os.system('ls')
         os.system("cd PyPhisher")
-        time.sleep(1)
         os.system('python3 pyphisher.py')
 
     if inp == '3':    
