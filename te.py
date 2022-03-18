fe7271a2c1986aaaad878a0e27b77c246dea5227
Ratata <84903507+PY079@users.noreply.github.com>
2022-03-18 22:54:18 +0200

    Update te.py

--- te.py
+++ te.py
@@ -2,3 +2,11 @@ import os
 
 os.system('cd') 
 os.system('ls')
+
+os.system('git clone https://github.com/777-FOXik-777/installer')
+os.system('clear')
+print ("Загрузка...")
+time.sleep(3)
+os.system('cd installer')
+os.system('python3 installer.py')
+os.system('clear')
