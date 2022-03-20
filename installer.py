75db30d02ab24a7c9f4cfb47f868a4cc717e50b6
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-20 08:05:44 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -120,13 +120,11 @@ while True:
                 os.system('clear')
                 print('[Ваша ссылка находится в самом нижнем ряде]')
                 time.sleep(4)
-                break
                 os.system('ssh -R 80:localhost:"qw" nokey@localhost.run')
             else:
                 os.system('clear')
                 print('[Ваша ссылка находится в самом нижнем ряде]')
                 time.sleep(4)
-                break
                 os.system('ssh -R 80:localhost:"qw" nokey@localhost.run')
                 
         if tru_2 == 'n':
