f5c92f69c4a8971dd03d8438b4be9d0392fe6c23
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-22 22:38:08 +0200

    Update fail.py

--- fail.py
+++ fail.py
@@ -28,7 +28,7 @@ while True:
     res()
     time.sleep(1)
     print ('\n')
-    print (Fore.YELLOW+"    [1] установить php ["+we+")
+    print (Fore.YELLOW+'    [1] установить php ['+we)
     print (Fore.YELLOW+"    [2] ")
     print (Fore.YELLOW+"    [3] ")
     print (Fore.YELLOW+"    [4] ")
