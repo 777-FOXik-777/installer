8ab9ebfeda032a6744d61665567aab37ca085a04
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-22 22:16:34 +0200

    Update zabava.py

--- zabava.py
+++ zabava.py
@@ -68,17 +68,17 @@ while True:
 
 
     if inp == '1':
-        os.system(sl)
+        os.system(cmatrix)
 
 
     if inp == '2':
         os.system(sl)
 
     if inp == '3':
-        os.system(sl)
+        os.system(telnet mapscii.me)
 
     if inp == '4':
-        os.system(sl)
+        os.system(cacafire)
 
 
     if inp == '77':
