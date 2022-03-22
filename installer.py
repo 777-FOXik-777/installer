05eef291d158b78f39040eaa4abc1fe873d0a35b
Ratata <84903507+PY079@users.noreply.github.com>
2022-03-22 21:18:46 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -66,45 +66,14 @@ while True:
         os.system('clear')
         print(Fore.RED+'[меню <забавы>]')
         time.sleep(3)
-        print('\n')
-        print(Fore.GREEN+'')
-        print (" #                 #          ##    ##              ")
-        print ("                   #           #     #              ")
-        print ("##   ###    ###   ###    ###   #     #     ##   ### ")
-        print (" #   #  #  ##      #    #  #   #     #    # ##  #  #")
-        print (" #   #  #    ##    #    # ##   #     #    ##    #   ")
-        print ("###  #  #  ###      ##   # #  ###   ###    ##   #   ")
-        print ('\n')
-        print ("[telegram: @SYPEXHACK]                      [v1.7.5]")
-        res()
-        time.sleep(1)
-        print ('\n')
-        print (Fore.YELLOW+"    [1] тунелирование <ngrok>")
-        print (Fore.YELLOW+"    [2] тунелирование <localhost>")
-        print (Fore.YELLOW+"    [3] фишинг <PyPhisher>")
-        print (Fore.YELLOW+"    [4] ддос <Doser>")
-        print (Fore.YELLOW+"    [5] заблокировать Termux <!!ОПАСНО!!>")
-        print (Fore.YELLOW+"    [6] просмотр взломаных камер <CAM-HACKERS>")
-        print (Fore.YELLOW+"    [7] получить вирус ссылку <android>")
-        print (Fore.YELLOW+"    [8] замаскировать фишинг <maskphish>")
-        print (Fore.YELLOW+"    [9] пробив по номеру <phoneinfoga>")
-        print (Fore.YELLOW+"    [10] пробив по ip <Uti-lite>")
-        print (Fore.CYAN+"\n                                     ╔═════════════╗")
-        print (Fore.CYAN+'    [77] поддержка автора            ║'+Fore.YELLOW+' утилиты '+Fore.CYAN+'[91]║')
-        print (Fore.CYAN+'    [66] тг канал автора             ║'+Fore.GREEN+'>забавы<'+Fore.CYAN+' [92]║')
-        print (Fore.CYAN+'    [99] обновить[BETA]              ║'+Fore.YELLOW+' файлы  '+Fore.CYAN+' [93]║')
-        print (Fore.CYAN+'    [00] выход                       ╚═════════════╝')
-        res()
-        inp_2 = input ('  Выбери пункт>>> ')
-        os.system('clear')
-
+        os.system('python te.py')
+        
+        
         if inp_2 == '91':
             os.system('clear')
             print(Fore.RED+'[меню <утилиты>]')
             time.sleep(3)
 
-        if inp_2 =='92':
-            os.system('clear')
 
 
 
