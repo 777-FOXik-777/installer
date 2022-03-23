bafbd414357cf14d670c91c73f2be9fe36c06cb5
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-23 17:21:18 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,3 +1,19 @@
+os.system('clear')
+print ('[установка нужных файлов]')
+time.sleep(3)
+os.system('clear')
+os.system('pkg install cmatrix')
+
+os.system('pkg install sl')
+os.system('pkg install tty-clock')
+
+os.system('clear')
+print ('[установка нужных файлов]')
+time.sleep(3)
+os.system('clear')
+
+os.system('pkg install libcaca -y')
+os.system('pkg install toilet')
 
 
 
