9e55a96109347c16537fabbad302b6c62d7c015a
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-23 17:23:18 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,4 +1,20 @@
+import os, time, webbrowser
+    
+os.system('clear')
+print ('[установка нужных файлов]')
+time.sleep(3)
+os.system('clear')
 
+#питон2
+os.system('pkg install python2')
+os.system('clear')
+print ('[установка нужных файлов]')
+time.sleep(3)
+os.system('clear')
+
+#доп.файл
+os.system('pip install colorama')
+os.system('clear')
 
 os.system('clear')
 print ('[установка нужных файлов]')
@@ -17,16 +33,5 @@ os.system('clear')
 os.system('pkg install libcaca -y')
 os.system('pkg install toilet')
 
-
-
-
-
-
-
-
-
-
-
-
-
+os.system('clear')
 os.system('python3 tool.py')
