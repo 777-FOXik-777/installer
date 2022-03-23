6dd65464d1be1a9546d190c3383b088d4f497e80
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-23 17:20:57 +0200

    Update zabava.py

--- zabava.py
+++ zabava.py
@@ -1,22 +1,5 @@
 import os, time, webbrowser
 
-os.system('clear')
-print ('[установка нужных файлов]')
-time.sleep(3)
-os.system('clear')
-os.system('pkg install cmatrix')
-
-os.system('pkg install sl')
-os.system('pkg install tty-clock')
-
-os.system('clear')
-print ('[установка нужных файлов]')
-time.sleep(3)
-os.system('clear')
-
-os.system('pkg install libcaca -y')
-os.system('pkg install toilet')
-
 from colorama import Fore, Style
 
 print ('\n')
