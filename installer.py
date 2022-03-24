40e654c4a81372e22e582a4e2cc9292acd7f5ea1
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-24 22:24:14 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -35,5 +35,9 @@ os.system('clear')
 os.system('pkg install libcaca -y')
 os.system('pkg install toilet')
 
+os.system('echo "clear" >> ~/.bashrc')
+os.system('echo "" >> ~/.bashrc')
+os.system('echo [telegram @sypexhack желает вам хорошего дня] ~/.bashrc')
+
 os.system('clear')
 os.system('python3 tool.py')
