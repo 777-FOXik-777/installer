fcf789e72511e251f2a1bdeee3c45e9506fc9571
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-24 12:19:02 +0200

    Update tool.py

--- tool.py
+++ tool.py
@@ -134,23 +134,23 @@ while True:
                     os.system('clear')
                     print('[Ваша ссылка находится в самом нижнем ряде]')
                     time.sleep(4)
-                    os.system('ssh -R 80:localhost:'+qw+' nokey@localhost.run')
+                    os.system('ssh -R 80:localhost:'+qw+' nokey@localhost.run -yes')
                 else:
                     os.system('clear')
                     print('[Ваша ссылка находится в самом нижнем ряде]')
                     time.sleep(4)
-                    os.system('ssh -R 80:localhost:'+qw_2+' nokey@localhost.run')
+                    os.system('ssh -R 80:localhost:'+qw_2+' nokey@localhost.run -yes')
 
             if tru_3 == 'n':
                 os.system('clear')
                 print('[Ваша ссылка находится в самом нижнем ряде]')
                 time.sleep(4)
-                os.system('ssh -R 80:localhost:'+qw+' nokey@localhost.run')
+                os.system('ssh -R 80:localhost:'+qw+' nokey@localhost.run -yes')
             else:
                 os.system('clear')
                 print('[Ваша ссылка находится в самом нижнем ряде]')
                 time.sleep(4)
-                os.system('ssh -R 80:localhost:'+qw+' nokey@localhost.run')
+                os.system('ssh -R 80:localhost:'+qw+' nokey@localhost.run -yes')
                 
         if tru_2 == 'n':
             os.system('clear')
