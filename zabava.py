6ccac336e08554e559b7691e83e3b920814e4fa5
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-25 19:30:49 +0200

    Update zabava.py

--- zabava.py
+++ zabava.py
@@ -16,7 +16,7 @@ while True:
     print ("| | | | | | \__ \ | |_  | (_| | | | | | |  __/ | |   ")
     print ("|_| |_| |_| |___/  \__|  \__,_| |_| |_|  \___| |_|   ")
     print ("\n")
-    print ("[telegram: @SYPEXHACK]                       [v2.3.1]")
+    print ("[telegram: @SYPEXHACK]                       [v2.4.0]")
     res()
     print (Fore.YELLOW+"    [1] матрица ")
     print (Fore.YELLOW+"    [2] паравозик")
