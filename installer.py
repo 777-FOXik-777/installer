e80780b903fee3d60d28b2fb4778fa256b98983a
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-26 20:51:39 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,40 +1,40 @@
 import os, time, webbrowser
-    
-os.system('clear')
-print ('[установка нужных файлов]')
-time.sleep(3)
-os.system('clear')
-
-os.system('pkg install python2')
-os.system('clear')
-print ('[установка нужных файлов]')
-time.sleep(3)
-os.system('clear')
 
 os.system('pip install colorama')
 os.system('clear')
+    
+from colorama import Fore, Style
+
+print ('\n')
+def res():
+    print(Style.RESET_ALL)
 
 os.system('clear')
-print ('[установка нужных файлов]')
-time.sleep(3)
+print ("[~] Установка питон'v2'...")
+os.system('pkg install python2')
 os.system('clear')
+
+
+print ('[~] Установка забав...')
 os.system('pkg install cmatrix')
 
 os.system('pkg install sl')
 os.system('pkg install tty-clock')
 
-os.system('clear')
-print ('[установка нужных файлов]')
-time.sleep(3)
-os.system('clear')
-
 os.system('pkg install libcaca -y')
+
+
+os.system('clear')
+print('[~] Установка приветствия...')
 os.system('pkg install toilet')
 
 os.system('rm ~/.bashrc')
 os.system('echo "clear" >> ~/.bashrc')
 os.system('echo "toilet -f mono9 -F metal hello" >> ~/.bashrc')
 os.system('echo "echo [telegram @sypexhack желает вам хорошего дня]" >> ~/.bashrc')
+os.system?'echo "echo изменить приветствие можно в installer пункт [5]" >> ~/.bashrc')
 
 os.system('clear')
+print('[Запуск installer]')
+time.sleep(3)
 os.system('python3 tool.py')
