33e3d8495e5b2437e9c61277e7d56d40b2a44882
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-03-27 11:12:28 +0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -14,23 +14,23 @@ def res():
 os.system('clear')
 print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка питон'v2'...")
 res()
+time.sleep(1.5)
 os.system('pkg install python2')
 os.system('clear')
 
 
 print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка забав...")
 res()
+time.sleep(1.5)
 os.system('pkg install cmatrix')
-
 os.system('pkg install sl')
 os.system('pkg install tty-clock')
-
 os.system('pkg install libcaca -y')
-
 
 os.system('clear')
 print(Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка приветствия...")
 res()
+time.sleep(1.5)
 os.system('pkg install toilet')
 
 os.system('rm ~/.bashrc')
