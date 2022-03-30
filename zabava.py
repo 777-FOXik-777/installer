3256fd4f0e1e446d0ca458d0bf49b4faa9a69fda
Ratata <84903507+PY079@users.noreply.github.com>
2022-03-30 21:41:21 +0300

    Update zabava.py

--- zabava.py
+++ zabava.py
@@ -9,7 +9,7 @@ def res():
 while True:
     os.system('clear')
     print(Fore.RED+'[меню <забавы>]')
-    print(Fore.GREEN+'')
+    print(Fore.GREEN+'', Style.BRIGHT)
     print (" _                 _             _   _               ")
     print ("(_)  _ __    ___  | |_    __ _  | | | |   ___   _ __ ")
     print ("| | | '_ \  / __| | __|  / _` | | | | |  / _ \ | '__|")
