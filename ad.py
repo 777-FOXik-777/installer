78ee755e4b9f6ea4d49b26cbe9fa3dec1c393e0c
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-02 23:04:47 +0300

    Create ad.py

---
+++ ad.py
@@ -0,0 +1,12 @@
+import os, time, webbrowser
+
+from colorama import Fore, Style
+
+print ('\n')
+def res():
+    print(Style.RESET_ALL)
+    
+while True:
+    os.system('clear')
+    print(Back.RED)
+    print(Fore.BLACK + 'тест')
