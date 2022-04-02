c043d44a999a340d514af8907fb0db4669eaed98
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-02 23:15:56 +0300

    Update ad.py

--- ad.py
+++ ad.py
@@ -5,16 +5,17 @@ from colorama import Fore, Style, Back
 print ('\n')
 def res():
     print(Style.RESET_ALL)
-    
+
+os.system('clear')
+print('Добро пожаловать в..')
+time.sleep(3)
+
 while True:
-    os.system('clear')
-    print('Добро пожаловать в..')
-    time.sleep(3)
     print(Back.RED)
     print(Fore.BLACK+'АДСКИЙ TERMUX')
     res()
-    print(FORE.YELLOW+'[1] сыграть')
-    print(FORE.YELLOW+'[0] выйти')
+    print(Fore.YELLOW+'[1] сыграть')
+    print(Fore.YELLOW+'[0] выйти')
     tsu = input('выберете пункт>>> ')
     if tsu == '1':
         os.system
