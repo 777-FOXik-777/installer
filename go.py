ee8086417edabd63512188053240dc81e0f8ccda
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-05 20:47:12 +0300

    Create go.py

---
+++ go.py
@@ -0,0 +1,45 @@
+import os, time, webbrowser
+
+from colorama import Fore, Style
+
+print ('\n')
+def res():
+    print(Style.RESET_ALL)
+
+os.system('clear')
+print('t')
+time.sleep(0.1)
+
+os.system('clear')
+print('te')
+time.sleep(0.1)
+
+os.system('clear')
+print('tel')
+time.sleep(0.1)
+
+os.system('clear')
+print('tele')
+time.sleep(0.1)
+
+os.system('clear')
+print('teleg')
+time.sleep(0.1)
+
+os.system('clear')
+print('telegr')
+time.sleep(0.1)
+
+os.system('clear')
+print('telegra')
+time.sleep(0.1)
+
+os.system('clear')
+print('telegram:')
+time.sleep(0.1)
+
+os.system('clear')
+print('telegram: '+Fore.GREEN+'@SYPEXHACK')
+time.sleep(99.1)
+
+1
