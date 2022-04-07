263626d5019dc6891498a6b18b885bf99a7fa666
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-07 09:35:02 +0300

    Update tool.py

--- tool.py
+++ tool.py
@@ -438,7 +438,7 @@ while True:
             os.system('cd && rm -fr installer && rm -fr installer.py && rm -fr tool.py && rm -fr fail.py && rm -fr zabava.py && git clone https://github.com/777-FOXik-777/installer && cd installer && python3 tool.py')
         if tru_008 == '2':
             os.system('clear')
-            print('Пример IP [33.73.133.137]')
+            print(Fore.YELLOW+'Пример IP 33.73.133.137')
             res()
             tsu_808 = input('введите ip >>> ')
             os.system('trace -t '+tsu_808)
