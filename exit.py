5e200df447a9fa3877bb0da5c822005e19b57953
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-10 19:52:04 +0300

    Update exit.py

--- exit.py
+++ exit.py
@@ -1,1 +1,12 @@
+import os, time, webbrowser
 
+from colorama import Fore, Style
+
+print ('\n')
+def res():
+    print(Style.RESET_ALL)
+
+os.system('clear')
+pritn(Fore.YELLOW+'Спасибо за использование installer')
+res()
+pritn(Fore.YELLOW+'подпишись на наш телеграм: @SYPEXHACK')
