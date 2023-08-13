759b248560040a752a723338e64f10221c779d36
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-08-13 16:02:07 +0300

    Update tool.py

--- tool.py
+++ tool.py
@@ -502,7 +502,7 @@ while True:
 
 
     if inp == '99':    
-        os.system('xdg-open https://t.me/MR_FOXik')
+        os.system('xdg-open https://t.me/perehodos')
         os.system('clear')
         
 
