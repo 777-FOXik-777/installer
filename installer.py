17291bbe5225a2ac9c4a9fb1b3abbf84169a0fdc
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-10-30 17:01:21 +0200

    installer.py

--- installer.py
+++ installer.py
@@ -24,36 +24,6 @@ res()
 time.sleep(1.5)
 os.system('pkg install python3')
 
-#забавы
-
-os.system('clear')
-print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка cmatrix...")
-res()
-time.sleep(1.5)
-os.system('pkg install cmatrix')
-os.system('clear')
-print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка sl...")
-res()
-time.sleep(1.5)
-os.system('pkg install sl')
-os.system('clear')
-print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка tty-clock...")
-res()
-time.sleep(1.5)
-os.system('pkg install tty-clock')
-os.system('clear')
-print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка libcaca...")
-res()
-time.sleep(1.5)
-os.system('pkg install libcaca -y')
-
-
-os.system('clear')
-print(Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка toilet...")
-res()
-time.sleep(1.5)
-os.system('pkg install toilet -y')
-
 #запуск
 
 os.system('clear')
