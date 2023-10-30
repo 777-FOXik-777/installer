db56dd47f5007c157c5386e97da5f54696e3e9aa
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-10-30 20:24:11 +0200

    tool.py

--- tool.py
+++ tool.py
@@ -54,7 +54,7 @@ while True:
             os.system('npm install ngrok -g')
             we  = '8080'
             os.system('clear')
-            print(Fore.CYAN+' Стандартный порт ['+Fore.YELLOW+'+8080+'+Fore.CYAN+']')
+            print(Fore.CYAN+' Стандартный порт ['+Fore.YELLOW+'8080'+Fore.CYAN+']')
             print(Fore.WHITE+'')
             tru_102 = input('\n'' Изменить порт? [y/n] >>> ')
             if tru_102 == 'y':
@@ -63,7 +63,7 @@ while True:
                 if we_2 == '':
                     os.system('clear')
                     print(Fore.YELLOW+' Вы ничего не ввели!')
-                    print('\n'+Fore.CYAN+' Использую стандартный порт ['+Fore.YELLOW+'+we+'+Fore.CYAN+']')
+                    print('\n'+Fore.CYAN+' Использую стандартный порт ['+Fore.YELLOW+'8080'+Fore.CYAN+']')
                     time.sleep(7)
                     os.system('clear')
                     os.system('ngrok http '+we+' && clear')
