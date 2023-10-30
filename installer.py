f9f6dde19dc0c2bdd9868427c8a3ece9f3d02c22
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-10-30 18:14:46 +0200

    installer.py

--- installer.py
+++ installer.py
@@ -1,25 +1,26 @@
 import os, time
 
-#колорам
-
 os.system('clear')
-print ("Установка colorama...")
-time.sleep(1.5)
 os.system('pip install colorama')
+os.system('clear')
 
 from colorama import Fore, Style
 
-print(Style.RESET_ALL)
-
+print ('\n')
+def res():
+    print(Style.RESET_ALL)
+  
 #питон
 
 os.system('clear')
 print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка python2...")
+res()
 time.sleep(1.5)
 os.system('pkg install python2')
 os.system('clear')
 
 print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка python3...")
+res()
 time.sleep(1.5)
 os.system('pkg install python3')
 
