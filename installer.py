73e885f653185784b997135ac511856577761e27
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-10-30 18:11:41 +0200

    installer.py

--- installer.py
+++ installer.py
@@ -4,7 +4,6 @@ import os, time
 
 os.system('clear')
 print ("Установка colorama...")
-res()
 time.sleep(1.5)
 os.system('pip install colorama')
 
@@ -16,12 +15,11 @@ print(Style.RESET_ALL)
 
 os.system('clear')
 print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка python2...")
-res()
 time.sleep(1.5)
 os.system('pkg install python2')
 os.system('clear')
+
 print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка python3...")
-res()
 time.sleep(1.5)
 os.system('pkg install python3')
 
