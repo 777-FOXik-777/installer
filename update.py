3b635acf5dfcf8d2eeec7ab7ff65e0bc0fa40fc7
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-10-31 17:41:36 +0200

    Update update.py

--- update.py
+++ update.py
@@ -8,4 +8,7 @@ os.system('cd')
 
 os.system('rm -fr installer')
 
+print('')
+print(Fore.YELLOW+" ["+Fore.RED+"i"+Fore.YELLOW+"] Установка Localhost...")
+
 os.system('git clone https://github.com/777-FOXik-777/installer && cd installer && python installer.py')
