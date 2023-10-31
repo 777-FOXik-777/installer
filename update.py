76062e593d733ad2a08116e97df105dbe15a2c03
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-10-31 17:23:36 +0200

    Create update.py

---
+++ update.py
@@ -0,0 +1,9 @@
+import os, time, webbrowser
+
+from colorama import Fore, Style
+
+os.system('clear')
+
+os.system('rm -fr installer')
+
+os.system('git clone https://github.com/777-FOXik-777/installer && cd installer && python installer.py')
