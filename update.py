763436eefb49768f8b5f4a5476d6f84114195e64
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-10-31 17:32:16 +0200

    Update update.py

--- update.py
+++ update.py
@@ -4,6 +4,8 @@ from colorama import Fore, Style
 
 os.system('clear')
 
+os.system('cd')
+
 os.system('rm -fr installer')
 
 os.system('git clone https://github.com/777-FOXik-777/installer && cd installer && python installer.py')
