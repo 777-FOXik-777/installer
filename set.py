02e6994e6b367ac6f606a29c4e2a0effd6362ec0
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-15 22:32:35 +0200

    Update set.py

--- set.py
+++ set.py
@@ -20,7 +20,7 @@ def baner():
     
 while True:
     baner()
-    print(Style.BRIGHT, Fore.CYAN+"[GitHub:  "+Fore.YELLOW+"https://github.com/777-FOXik-777/installer"+Fore.CYAN+"]")
+    print(Style.BRIGHT, Fore.CYAN+"[GitHub:   "+Fore.YELLOW+"https://github.com/777-FOXik-777/installer"+Fore.CYAN+"]")
     print(Fore.GREEN+"\n    [Настройки]")
     res()
     print(Fore.GREEN+"    [1] Запускать Installer вместе с Termux")
