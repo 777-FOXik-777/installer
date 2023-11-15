1ca16ed369a675c64272066481b12699f620bdbe
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-15 22:38:29 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,61 +1,60 @@
 import os, time
 
-if inp == '8':
-    filename = "tg_SYPEXHACK"
+filename = "tg_SYPEXHACK"
 
-    if os.path.exists(filename):
-           
-        #colorama
-        
-        os.system('clear')
-        os.system('cd /data/data/com.termux/files/home/installer/installer/')
-        
-        print ('[~] Установка colorama... \n')
-        time.sleep(1.5)
-        os.system('pip install colorama')
-        os.system('clear')
-        
-        from colorama import Fore, Style
-        
-        print ('\n')
-        def res():
-            print(Style.RESET_ALL)
-          
-        #питон
-        
-        os.system('clear')
-        print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка python2...")
-        res()
-        time.sleep(1.5)
-        os.system('pkg install python2')
-        os.system('clear')
-        
-        print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка python3...")
-        res()
-        time.sleep(1.5)
-        os.system('pkg install python3')
-        
-        #доступ к файлам
-        
-        os.system('clear')
-        print (Fore.YELLOW+"["+Fore.CYAN+"!"+Fore.YELLOW+"] Разрешите доступ к файлам")
-        res()
-        time.sleep(1.5)
-        os.system('termux-setup-storage')
-        tsu = input('\n[Нажмите enter чтобы продолжить]')
-        os.system('clear')
+if os.path.exists(filename):
+       
+    #colorama
+    
+    os.system('clear')
+    os.system('cd /data/data/com.termux/files/home/installer/installer/')
+    
+    print ('[~] Установка colorama... \n')
+    time.sleep(1.5)
+    os.system('pip install colorama')
+    os.system('clear')
+    
+    from colorama import Fore, Style
+
+    print ('\n')
+    def res():
+        print(Style.RESET_ALL)
+      
+    #питон
+    
+    os.system('clear')
+    print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка python2...")
+    res()
+    time.sleep(1.5)
+    os.system('pkg install python2')
+    os.system('clear')
+    
+    print (Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Установка python3...")
+    res()
+    time.sleep(1.5)
+    os.system('pkg install python3')
+    
+    #доступ к файлам
+    
+    os.system('clear')
+    print (Fore.YELLOW+"["+Fore.CYAN+"!"+Fore.YELLOW+"] Разрешите доступ к файлам")
+    res()
+    time.sleep(1.5)
+    os.system('termux-setup-storage')
+    tsu = input('\n[Нажмите enter чтобы продолжить]')
+    os.system('clear')
 
 
-        os.system('rm -fr tg_SYPEXHACK')
-        
-        #запуск
-        
-        os.system('clear')
-        os.system('xdg-open https://t.me/+p9CBKAxQUQZmMjli')
-        os.system('python3 tool.py')
-        
+    os.system('rm -fr tg_SYPEXHACK')
+    
+    #запуск
+    
+    os.system('clear')
+    os.system('xdg-open https://t.me/+p9CBKAxQUQZmMjli')
+    os.system('python3 tool.py')
+    
 
-    else:
-        os.system('clear')
-        os.system('python3 tool.py')
+else:
+    os.system('clear')
+    os.system('python3 tool.py')
         
