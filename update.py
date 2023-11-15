de0c0688983abd5afb00ed0964157dc356277f9d
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-15 22:23:36 +0200

    Update update.py

--- update.py
+++ update.py
@@ -6,7 +6,9 @@ os.system('clear')
 
 os.system('cd')
 
+os.system('rm -fr /data/data/com.termux/files/home/installer')
 os.system('rm -fr installer')
+os.chdir('/data/data/com.termux/files/home/')
 
 print(Fore.YELLOW+" ["+Fore.RED+"i"+Fore.YELLOW+"] Обновление/Переустановление Installer...")
 time.sleep(1.5)
