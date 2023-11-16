f85d3ed67760d3ca1fe764f3dce3651453a0a82e
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-16 22:43:28 +0200

    Update tool.py

--- tool.py
+++ tool.py
@@ -2,6 +2,7 @@ import os, time
 
 from colorama import Fore, Style
 
+os.system('rm -fr /data/data/com.termux/files/home/installer/trash/tg_SYPEXHACK')
 os.system('rm -fr /data/data/com.termux/files/home/update.py')
 os.system('rm -fr /data/data/com.termux/files/home/installer/tg_SYPEXHACK')
 
