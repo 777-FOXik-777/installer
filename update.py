ad14bacf0ad390a28a347f6f82b0649e62308f9e
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-19 23:21:16 +0200

    Update update.py

--- update.py
+++ update.py
@@ -12,8 +12,15 @@ print(Fore.YELLOW+" ["+Fore.RED+"i"+Fore.YELLOW+"] Обновление/Пере
 time.sleep(1.5)
 print(Fore.WHITE+"")
 
-os.system('git clone https://github.com/777-FOXik-777/installer && cd installer')
+os.system('git clone https://github.com/777-FOXik-777/installer')
 
-os.system('rm ~/.bashrc')
+filename = "installer"
 
-os.system('python tool.py')
+if os.path.exists(filename):
+  os.system('rm ~/.bashrc')
+  os.system('cd installer && python tool.py')
+  
+else:
+  os.system('clear')
+  print(Fore.YELLOW+" ["+Fore.RED+"!"+Fore.YELLOW+"] Ошибка во время обновления!")
+   print(Fore.WHITE+" ")
