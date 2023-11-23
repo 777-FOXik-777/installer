388fdac224761807521d6b5f158e08d7c0c7f86a
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-23 21:07:19 +0200

    Update tool.py

--- tool.py
+++ tool.py
@@ -8,7 +8,7 @@ os.system('rm -fr /data/data/com.termux/files/home/update.py')
 os.system('cd /data/data/com.termux/files/home/installer/')
 os.system('clear')
 
-print(f'\33]0;Installer made with love - telegram: @SYPEXHACK\a',
+print(f'\33]0; Installer made with love\a',
                   end='', flush=True)
 
 def res():
