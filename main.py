2e31198d728f4dc4f5330e17fae95a7571cda989
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-26 21:33:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for i in range(10):
     # Читаем вывод команды SSH
     output = p.stdout.read().decode('utf-8')
     # Ищем URL в выводе
-    match = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
+    match = re.search("(.lhr.life)", output)
     if match:
         cf_url = match.group(0)
         cf_success = True
