446d00ecfa5a01cc217a102f04fbfadb872379e5
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-26 20:40:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,18 @@ import time
 import re
 import subprocess
 
-# Запускаем команду SSH в фоновом режиме
-p = subprocess.Popen(["ssh", "-R", "80:localhost:8080", "localhost.run", "-T", "-n"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+def bgtask(command, stdout=PIPE, stderr=DEVNULL, cwd="./"):
+    try:
+        return Popen(command, shell=True, stdout=stdout, stderr=stderr, cwd=cwd)
+    except Exception as e:
+        append(e, error_file)
 
-cf_success = False
-cf_url = ""
-for i in range(10):
-    # Читаем вывод команды SSH
-    output = p.stdout.read().decode('utf-8')
-    # Ищем URL в выводе
-    match = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
-    if match:
-        cf_url = match.group(0)
-        cf_success = True
-        break
-    time.sleep(1)
-
-print(f'\n[~] Link: {cf_url}')
+bgtask("ssh -R 80:localhost:8080 localhost.run -T -n", stdout=cf_log, stderr=cf_log)
+    cf_success = False
+    for i in range(10):
+        cf_url = grep("(https://[-0-9a-z.]*.lhr.life)", cf_file)
+        if cf_url != "":
+            cf_success = True
+            break
+        time.sleep(1)
+    print(f'\n[~] Link: {cf_url}')
