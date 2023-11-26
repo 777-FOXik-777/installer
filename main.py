20a1742bd9df65952a25c5fbc966d44f00a33f65
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-26 20:12:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import subprocess
-import time
-
 cmd = "ssh -R 80:localhost:8080 nokey@localhost.run"
 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 cf_success = False
 cf_url = ""
 for i in range(10):
-    line = p.stdout.readline().decode('utf-8')
-    if line.endswith(".lhr.life\n"):
-        cf_url = line.strip()
+    output = p.stdout.read().decode('utf-8')
+    lines = output.split('\n')
+    last_line = lines[-1]
+    if "https://" in last_line:
+        cf_url = last_line.strip()
         cf_success = True
         break
     time.sleep(1)
