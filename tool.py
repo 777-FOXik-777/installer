0eaaf72a4a639b24c3c2f8d9de331e9fc4865873
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-26 17:20:53 +0200

    Update tool.py

--- tool.py
+++ tool.py
@@ -8,7 +8,7 @@ os.system('rm -fr /data/data/com.termux/files/home/update.py')
 os.system('cd /data/data/com.termux/files/home/installer/')
 os.system('clear')
 
-def pri()
+def pri():
     print(f'\33]0; Installer - Страница [1]\a',
                       end='', flush=True)
 
