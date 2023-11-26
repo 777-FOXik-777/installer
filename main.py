d076574c288d161ee51ecc306218100f3b9e205e
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-26 20:43:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ import time
 import re
 import subprocess
 
-def bgtask(command, stdout=PIPE, stderr=DEVNULL, cwd="./"):
+def bgtask(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, cwd="./"):
     try:
-        return Popen(command, shell=True, stdout=stdout, stderr=stderr, cwd=cwd)
+        return subprocess.Popen(command, shell=True, stdout=stdout, stderr=stderr, cwd=cwd)
     except Exception as e:
-        append(e, error_file)
+        print(e)
 
-bgtask("ssh -R 80:localhost:8080 localhost.run -T -n", stdout=cf_log, stderr=cf_log)
-    cf_success = False
-    for i in range(10):
-        cf_url = grep("(https://[-0-9a-z.]*.lhr.life)", cf_file)
-        if cf_url != "":
-            cf_success = True
-            break
-        time.sleep(1)
-    print(f'\n[~] Link: {cf_url}')
+# Запускаем команду SSH в фоновом режиме
+p = bgtask("ssh -R 80:localhost:8080 localhost.run -T -n")
+
+cf_success = False
+cf_url = ""
+for i in range(10):
+    # Читаем вывод команды SSH
+    output = p.stdout.read().decode('utf-8')
+    # Ищем URL в выводе
+    match = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
+    if match:
+        cf_url = match.group(0)
+        cf_success = True
+        break
+    time.sleep(1)
+
+print(f'\n[~] Link: {cf_url}')
