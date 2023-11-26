58235d9f75776482c748a254282b5a0c5a150b56
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-26 19:58:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import subprocess
+import time
+
+cmd = "ssh -R 80:localhost:8080 nokey@localhost.run"
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+cf_success = False
+cf_url = ""
+for i in range(10):
+    line = p.stdout.readline().decode('utf-8')
+    if "https://" in line:
+        cf_url = line.strip()
+        cf_success = True
+        break
+    time.sleep(1)
+
+print(f'\n[~] Link: {cf_url}')
