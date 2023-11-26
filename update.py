4770ceb2c86d6205437963dfafbad72562f42108
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-26 17:23:59 +0200

    Update update.py

--- update.py
+++ update.py
@@ -17,12 +17,13 @@ print(Fore.WHITE+"")
 
 os.system('git clone https://github.com/777-FOXik-777/installer')
 
+os.system('rm ~/.bashrc')
+
 filename = "installer"
 
 if os.path.exists(filename):
   
-  os.system('rm ~/.bashrc')
-  print(f'\33]0; Installer успешно\a',
+  print(f'\33]0; Installer - Успешно установлен!\a',
                   end='', flush=True)
   os.system('clear')
   print(Fore.YELLOW+" ["+Fore.GREEN+"i"+Fore.YELLOW+"] Installer успешно обновлен/переустановлен!")
@@ -34,10 +35,10 @@ if os.path.exists(filename):
 
 else:
   
-  print(f'\33]0; Installer ошибка во время обновления!\a',
+  print(f'\33]0; Installer - Ошибка во время обновления!\a',
                   end='', flush=True)
   os.system('clear')
   print(Fore.YELLOW+" ["+Fore.RED+"!"+Fore.YELLOW+"] Ошибка во время обновления Installer!")
   print(Fore.WHITE+" ")
-  print(Fore.YELLOW+" ["+Fore.GREEN+"i"+Fore.YELLOW+"] Создайте новый сезон в Termux чтобы повторить!")
+  print(Fore.YELLOW+" ["+Fore.GREEN+"i"+Fore.YELLOW+"] Чтобы повторить используйте команду "+Fore.WHITE+"python update.py "+Fore.YELLOW+"!")
   print(Fore.WHITE+" ")
