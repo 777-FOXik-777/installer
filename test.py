d898a1cf45096d60840b1c16199422e01158497a
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-27 17:18:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,33 +1,24 @@
 import subprocess
 import re
-import select
-import time
 
-def get_url(timeout=10):
-    # Запускаем команду SSH в фоновом режиме
+def bgtask(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL):
     try:
-        p = subprocess.Popen("ssh -R 80:8080 nokey@localhost.run -T -n", shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        return subprocess.Popen(command, shell=True, stdout=stdout, stderr=stderr)
     except Exception as e:
-        print(f"Ошибка при запуске команды SSH: {e}")
-        return ""
+        print(e)
 
-    # Читаем вывод команды SSH и ищем URL
-    start_time = time.time()
-    while True:
-        # Проверяем, не истекло ли время ожидания
-        if time.time() - start_time > timeout:
-            print("Время ожидания истекло")
-            return ""
+# Запускаем команду SSH в фоновом режиме
+p = bgtask("ssh -R 80:8080 nokey@localhost.run")
 
-        # Читаем из stdout, если есть что читать
-        if select.select([p.stdout], [], [], 0.0)[0]:
-            output = p.stdout.read().decode('utf-8')
-            match = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
+cf_url = ""
+for i in range(10):
+    # Читаем вывод команды SSH
+    output = p.stdout.read().decode('utf-8')
+    # Ищем URL в выводе
+    match = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
+    if match:
+        cf_url = match.group(0)
+        break
 
-            # Возвращаем URL или продолжаем ожидание, если URL не найден
-            if match:
-                return match.group(0)
+print(f'\n[~] Ссылка: {cf_url}')
 
-    return ""
-
-print(f'{get_url()}')
