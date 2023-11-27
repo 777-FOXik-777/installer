29227415fb44b47edcbe43691b3fd0f740df7b0d
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-27 18:56:23 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import subprocess
 import re
+import os
 
 def get_url():
     command = "ssh -R 80:localhost:8080 nokey@localhost.run"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
     while True:
+        os.system('clear')
         output = process.stdout.readline().decode('utf-8')
         url = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
         if url is not None:
