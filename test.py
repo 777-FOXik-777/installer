49d7be6657b730bfd319820b9f7c13bee02e373c
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-27 16:33:22 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import re
 
 def url():
     # Запускаем команду SSH в фоновом режиме
-    p = subprocess.Popen("ssh -R 80:8080 nokey@localhost.run -T -n", shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    p = subprocess.Popen("ssh -R 80:8080 nokey@localhost.run", shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
     # Читаем вывод команды SSH и ищем URL
     output = p.stdout.read().decode('utf-8')
