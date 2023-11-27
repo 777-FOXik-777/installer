978255a4cecbe4a55250926a4f80c43b8ed0e221
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-27 18:45:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -19,18 +19,15 @@ def get_url():
     command = "ssh -R 80:localhost:8080 nokey@localhost.run"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
-    while True:
-        output = process.stdout.readline().decode('utf-8')
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            os.system('clear')
-            url = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
-            if url is not None:
-                return url.group(1)
-        time.sleep(1)
-
-    return None
+    output = process.stdout.readline().decode('utf-8')
+    if output == '' and process.poll() is not None:
+        break
+    if output:
+        os.system('clear')
+        url = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
+        if url is not None:
+            return url.group(1)
+    time.sleep(1)
 
 url = get_url()
 if url is not None:
