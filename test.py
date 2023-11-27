f178e5ea42d4e183146d3cf4ac23aad27cfffd82
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-27 18:38:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,20 @@
 import subprocess
 import re
-import os
-
-
+import time
 
 def get_url():
     command = "ssh -R 80:localhost:8080 nokey@localhost.run"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    os.system('clear')
-    
-    if error is not None:
-        print(f"Произошла ошибка: {error}")
-        return None
 
-    url = re.search("(https://[-0-9a-z.]*.lhr.life)", output.decode('utf-8'))
-    if url is not None:
-        return url.group(1)
+    while True:
+        output = process.stdout.readline().decode('utf-8')
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            url = re.search("(https://[-0-9a-z.]*.lhr.life)", output)
+            if url is not None:
+                return url.group(1)
+        time.sleep(1)
 
     return None
 
@@ -25,3 +23,4 @@ if url is not None:
     print(f'URL: {url}')
 else:
     print("URL не найден")
+
