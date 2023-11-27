93bd3c7518d1e52032e5938ad30c9b2ce71b7f9b
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-27 17:45:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def setup():
     bgtask("ssh -R 80:localhost:8080 nokey@localhost.run")
     cf_success = False
     for i in range(10):
-        cf_url = "https://[-0-9a-z.]*.lhr.life"
+        cf_url = re.search("(https://[-0-9a-z.]*.lhr.life)")
         if cf_url != "":
             cf_success = True
             break
