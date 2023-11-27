2c89d0a2b592279a58b153ca1fc7e22c71eef0ec
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-27 17:52:27 +0200

    Update test.py

--- test.py
+++ test.py
@@ -26,13 +26,12 @@ def bgtask(command, stdout=PIPE, stderr=DEVNULL, cwd="./"):
 
 
 def setup():
-    time.sleep(2)
+    time.sleep(1)
     bgtask("ssh -R 80:localhost:8080 nokey@localhost.run")
     cf_success = False
     for i in range(10):
         cf_url = grep("(https://[-0-9a-z.]*.lhr.life)")
         if cf_url != "":
-            cf_success = True
             break
         time.sleep(1)
     print(f'\n[~] Link: {cf_url}')
