f161d2b351952c2e43b0e2d0ba3f4e04344655a3
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-11-27 14:47:07 +0200

    Update update.py

--- update.py
+++ update.py
@@ -11,7 +11,7 @@ os.system('rm -fr /data/data/com.termux/files/home/installer')
 os.system('rm -fr installer')
 os.chdir('/data/data/com.termux/files/home/')
 
-print(Fore.YELLOW+" ["+Fore.RED+"i"+Fore.YELLOW+"] Обновление/Переустановка Installer...")
+print(Fore.YELLOW+" ["+Fore.RED+"i"+Fore.YELLOW+"] Установка Installer...")
 time.sleep(1.5)
 print(Fore.WHITE+"")
 
@@ -26,7 +26,7 @@ if os.path.exists(filename):
   print(f'\33]0; Installer - Успешно установлен!\a',
                   end='', flush=True)
   os.system('clear')
-  print(Fore.YELLOW+" ["+Fore.GREEN+"i"+Fore.YELLOW+"] Installer успешно обновлен/переустановлен!")
+  print(Fore.YELLOW+" ["+Fore.GREEN+"i"+Fore.YELLOW+"] Installer успешно установлен!")
   print(Fore.WHITE+" ")
   tsu = input(' [Нажмите enter чтобы продолжить]')
   os.system('clear')
@@ -38,7 +38,7 @@ else:
   print(f'\33]0; Installer - Ошибка во время обновления!\a',
                   end='', flush=True)
   os.system('clear')
-  print(Fore.YELLOW+" ["+Fore.RED+"!"+Fore.YELLOW+"] Ошибка во время обновления Installer!")
+  print(Fore.YELLOW+" ["+Fore.RED+"!"+Fore.YELLOW+"] Ошибка во время установки Installer!")
   print(Fore.WHITE+" ")
-  print(Fore.YELLOW+" ["+Fore.GREEN+"i"+Fore.YELLOW+"] Повторите используя команду "+Fore.WHITE+"python update.py"+Fore.YELLOW+" !")
+  print(Fore.YELLOW+" ["+Fore.GREEN+"i"+Fore.YELLOW+"] Повторите используя команду:"+Fore.WHITE+" python update.py")
   print(Fore.WHITE+" ")
