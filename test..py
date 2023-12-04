0980583afb0fc2a8ff8ef0ac6c3063f4625aad52
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-12-04 22:35:54 +0200

    Create test..py

---
+++ test..py
@@ -0,0 +1,15 @@
+# Импортируем модуль re для работы с регулярными выражениями
+import re
+
+# Определяем шаблон для поиска URL, начинающегося с https://
+pattern = r"https://\S+"
+
+# Запускаем команду ssh с нужными параметрами и получаем ее вывод в виде строки
+output = subprocess.getoutput("ssh -R 80:localhost:8080 nokey@localhost.run  -T -n")
+
+# Ищем в выводе первое совпадение с шаблоном
+match = re.search(pattern, output)
+
+# Если совпадение найдено, выводим его на экран
+if match:
+    print(match.group())
