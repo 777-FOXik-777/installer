25f160bae97690ff8a84e1b6a5cb2a753785c4f1
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-12-04 23:05:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-# Импортируем модуль subprocess для запуска команды ssh
+# Импортируем модули subprocess и re для запуска команды ssh и работы с регулярными выражениями
 import subprocess
+import re
+
+# Задаем регулярное выражение для поиска ссылки
+regex = "(https://[-0-9a-z.]*.lhr.life)"
 
 # Запускаем команду ssh с нужными параметрами и получаем вывод в переменную output
 output = subprocess.run(["ssh", "-R", "80:localhost:8080", "nokey@localhost.run", "-T", "-n"], capture_output=True, text=True).stdout
 
-# Ищем в выводе ссылку, которая заканчивается на .lhr.life, и выводим ее на экран
-for line in output.splitlines():
-    if line.endswith(".lhr.life"):
-        print(line)
-        break
+# Используем функцию re.search для поиска первого совпадения регулярного выражения в выводе
+match = re.search(regex, output)
 
-
+# Проверяем, что совпадение найдено
+if match:
+    # Выводим найденную ссылку на экран
+    print(match.group(0))
+else:
+    # Выводим сообщение, что ссылка не найдена
+    print("Ссылка не найдена")
