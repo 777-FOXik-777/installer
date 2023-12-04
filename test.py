9a9fd6c1137a792364d94adc1643cf7eaf93636c
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-12-04 22:56:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+# Импортируем модули re и subprocess для работы с регулярными выражениями и запуска команды ssh
+import re
+import subprocess
+
 # Определяем имя файла, в который записывается вывод команды ssh
 cf_file = "logs/lh.log"
 
@@ -10,10 +14,17 @@ def grep(pattern, file):
                 return match.group(1)
     return ""
 
+# Определяем функцию для запуска команды в фоновом режиме и перенаправления ее вывода в файл
 def bgtask(command, stdout, stderr, cwd="./"):
-    # Пытаемся запустить команду в фоновом режиме и вернуть объект процесса
     try:
         return subprocess.Popen(command, shell=True, stdout=stdout, stderr=stderr, cwd=cwd)
+    except Exception as e:
+        # Если возникла ошибка, записываем ее в файл error.log
+        with open("error.log", "a") as file:
+            file.write(str(e) + "\n")
+
+# Открываем файл для записи вывода команды ssh
+cf_log = open(cf_file, "w")
 
 # Запускаем команду ssh в фоновом режиме и перенаправляем ее вывод в файл
 bgtask("ssh -R 80:localhost:8080 nokey@localhost.run -T -n", stdout=cf_log, stderr=cf_log)
@@ -29,3 +40,7 @@ for i in range(10):
 
 # Выводим найденный URL на экран
 print(f'\n[~] Link: {cf_url}')
+
+# Закрываем файл
+cf_log.close()
+
