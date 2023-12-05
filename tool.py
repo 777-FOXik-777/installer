bac6fdcd06c9b9bf6e56cffc01a58d5630b43560
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-12-05 20:33:21 +0200

    Update tool.py

--- tool.py
+++ tool.py
@@ -132,7 +132,7 @@ while True:
     os.chdir('/data/data/com.termux/files/home/installer')
     pri()
     baner()
-    print(Style.BRIGHT, Fore.CYAN+" [Telegram: @SYPEXHACK]                         ["+Fore.YELLOW+"2.10.1"+Fore.CYAN+"]")
+    print(Style.BRIGHT, Fore.CYAN+"[Telegram: @SYPEXHACK]                         ["+Fore.YELLOW+"2.10.1"+Fore.CYAN+"]")
     res()
     print(Fore.GREEN+"    [1] Ngrok      ("+Ngrok+""+Fore.GREEN+")  >>  Тунелирование")
     print(Fore.GREEN+"    [2] Localhost  ("+Localhost+""+Fore.GREEN+")  >>  Тунелирование")
@@ -149,7 +149,7 @@ while True:
     print(Fore.YELLOW+"    [s] Настройки")
     print(Fore.YELLOW+"    [e] Выход")
     res()
-    inp = input('  Выбери пункт>>> ')
+    inp = input(' Выбери пункт>>> ')
     os.system('clear')
 
     
