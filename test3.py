68ea9e6cd29f35f4547e1b48e56facad55a4ff4d
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-12-12 22:38:17 +0200

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,62 @@
+import os, time
+
+from colorama import Fore, Style
+
+os.system('cd /data/data/com.termux/files/home/installer/')
+os.system('clear')
+
+
+
+def pri():
+    print(f'\33]0; Installer - Страница [3]\a',
+                      end='', flush=True)
+
+def exit():
+    res()
+    exit = input('[Нажмите Enter чтобы выйти]')
+    os.chdir('/data/data/com.termux/files/home/installer')
+    os.system('clear')
+
+
+def res():
+    print(Style.RESET_ALL)
+
+
+def baner():
+    os.chdir('/data/data/com.termux/files/home/installer/banner')
+    os.system('clear')
+    print(Fore.CYAN+'', Style.BRIGHT)
+    os.system('lolcat baner.txt')
+    os.chdir('/data/data/com.termux/files/home/Installer_Files')
+
+
+#начало
+    
+while True:
+    os.chdir('/data/data/com.termux/files/home/installer/banner')
+    pri()
+    os.system('lolcat banner3.txt')
+    inp = input('\n Выбери пункт ➤ ')
+    os.system('clear')
+    
+
+
+    if inp == '21':
+        os.system('clear')
+
+
+    
+    if inp == 't':
+        os.system('xdg-open https://t.me/SYPEXHACK')
+        os.system('clear')
+
+    
+    
+    if inp == 's':
+        os.chdir('/data/data/com.termux/files/home/installer')
+        os.system('python set_test.py')
+
+    
+    
+    if inp == 'e':
+        break
