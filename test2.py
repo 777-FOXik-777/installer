d498efb0ef833c39ef2c6160f8ce4e70bebacda1
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-12-14 17:51:27 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -25,7 +25,6 @@ def res():
 def baner():
     os.chdir('/data/data/com.termux/files/home/installer/banner')
     os.system('clear')
-    print(Fore.CYAN+'', Style.BRIGHT)
     os.system('lolcat baner.txt')
     res()
     os.chdir('/data/data/com.termux/files/home/Installer_Files')
