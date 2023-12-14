3c5bb6447abd4596d34b71c873d98ed4c23726f0
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-12-14 17:47:11 +0200

    Update set_test.py

--- set_test.py
+++ set_test.py
@@ -66,7 +66,7 @@ while True:
             os.system('rm -fr /data/data/com.termux/files/home/Installer_Files/trash/Auto')
             os.system('clear')
             baner()
-            print(Fore.GREEN+"  Включено!")
+            print(Style.BRIGHT, Fore.GREEN+"Включено!")
             exit()
             
         if tru_101 == '2':
@@ -75,14 +75,14 @@ while True:
           
             if os.path.exists(filename):
               baner()
-              print(Fore.YELLOW+"  Авто-запуск еще не включен!")
+              print(Style.BRIGHT, Fore.YELLOW+"Авто-запуск еще не включен!")
               exit()
               
             else:
               os.system('rm ~/.bashrc')
               os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/Auto")
               baner()
-              print(Fore.YELLOW+"  Выключено!")
+              print(Style.BRIGHT, Fore.YELLOW+"Выключено!")
               exit()
             
         else:
