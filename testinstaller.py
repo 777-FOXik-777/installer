e704be7312e09bb5bd49cd4feced857792a1d9a9
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2023-12-15 15:36:40 +0200

    Update testinstaller.py

--- testinstaller.py
+++ testinstaller.py
@@ -152,8 +152,6 @@ else:
     time.sleep(1)
     os.system('termux-setup-storage')
     tsu = input(' [Нажмите Enter чтобы продолжить]')
-    os.system('mv /data/data/com.termux/files/home/installer/image/hack.jpg /sdcard/Pictures')
-    os.system('rm -fr /data/data/com.termux/files/home/installer/image')
     os.system('clear')
 
 
