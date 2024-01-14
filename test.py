08e4d577531d14438e4f92232d97a55b3472b201
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-14 20:53:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,34 +9,15 @@ def res():
     print(Style.RESET_ALL)
 
 
-def baner():
-    os.system('clear')
-    print(Fore.CYAN+'', Style.BRIGHT)
-    print("  ___                 _             _   _               ")
-    print(" |_ _|  _ __    ___  | |_    __ _  | | | |   ___   _ __ ")
-    print("  | |  | '_ \  / __| | __|  / _` | | | | |  / _ \ | '__|")
-    print("  | |  | | | | \__ \ | |_  | (_| | | | | | |  __/ | |   ")
-    print(" |___| |_| |_| |___/  \__|  \__,_| |_| |_|  \___| |_|   ")
-    res()
+os.chdir('/data/data/com.termux/files/home')
 
-def compare_filenames_in_folder(folder_path, filename_to_compare):
-    
-    os.chdir('/data/data/com.termux/files/home/Installer_Files/version')
-  
-    os.system('wget -O chek https://raw.githubusercontent.com/777-FOXik-777/installer/main/Installer_Files/version')
-  
-    
-    # Получаем список файлов в указанной папке
-    files_in_folder = os.listdir(folder_path)
+os.system('wget -O chek https://raw.githubusercontent.com/777-FOXik-777/installer/main/Installer_Files/version')
 
-    # Проверяем наличие файла с нужным именем в папке
-    if filename_to_compare in files_in_folder:
-        print(f"Обновления нету!")
-    else:
-        print(f"Есть обновление!")
 
-# Пример использования
-folder_path = "/data/data/com.termux/files/home/Installer_Files/version/chek/"
-filename_to_compare = "3.0.0"
+filename = "/data/data/com.termux/files/home/chek/3.0.0"
 
-compare_filenames_in_folder(folder_path, filename_to_compare)
+if os.path.exists(filename):
+  print("Обновелния нету!")
+
+else:
+  print("Есть обновление!")
