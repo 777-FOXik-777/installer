2e9d2a98ededd6d68d433d5dfee220b00db571ac
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-14 21:08:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ def res():
     print(Style.RESET_ALL)
 
 
-os.chdir('/data/data/com.termux/files/home/Installer_Files/version/')
+os.chdir('/data/data/com.termux/files/home/installer/Installer_Files/version/')
 
 os.system('wget https://raw.githubusercontent.com/777-FOXik-777/installer/main/Installer_Files/version/3.0.1')
 
 
-filename = "/data/data/com.termux/files/home/Installer_Files/version/3.0.1"
+filename = "/data/data/com.termux/files/home/installer/Installer_Files/version/3.0.1"
 
 if os.path.exists(filename):
   print("Есть обновлени!")
