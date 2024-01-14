dbb6479d1243f580ec2cb86c5acda894cb544a88
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-14 21:04:04 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def res():
 
 os.chdir('/data/data/com.termux/files/home')
 
-os.system('wget -O chek https://raw.githubusercontent.com/777-FOXik-777/installer/main/Installer_Files/version')
+os.system('wget -O chek https://raw.githubusercontent.com/777-FOXik-777/installer/main/Installer_Files/version/3.0.1')
 
 
 filename = "/data/data/com.termux/files/home/chek/3.0.0"
