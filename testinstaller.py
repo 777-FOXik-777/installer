fd862563dcf6ec1925c7da07ca15570985a9c8ae
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-17 12:15:36 +0200

    Update testinstaller.py

--- testinstaller.py
+++ testinstaller.py
@@ -405,6 +405,79 @@ time.sleep(3)
 
 
 
+#обновление
+
+os.system('clear')
+
+filename = "/data/data/com.termux/files/home/Installer_Files/trash/update"
+
+if os.path.exists(filename):
+  
+    os.system('clear')
+
+
+else:
+  
+    baner()
+    print (Fore.YELLOW+" ["+Fore.CYAN+"~"+Fore.YELLOW+"] Проверка наличий обновления...")
+    print(Fore.WHITE+'', Style.BRIGHT)
+    time.sleep(1)
+    
+    
+    os.chdir('/data/data/com.termux/files/home/Installer_Files/trash')
+    
+    
+    os.system('git clone --depth 1  https://github.com/777-FOXik-777/installer updatepak')
+    
+    def compare_files(file1_path, file2_path):
+        with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
+            return file1.read() == file2.read()
+    
+    
+    
+    # Пример использования
+    file1_path = "/data/data/com.termux/files/home/Installer_Files/trash/updatepak/Installer_Files/about/version"
+    file2_path = "/data/data/com.termux/files/home/Installer_Files/about/version"
+    
+    result = compare_files(file1_path, file2_path)
+    
+    if result:
+        os.system('rm -fr /data/data/com.termux/files/home/Installer_Files/trash/updatepak')
+        baner()
+        print (Fore.YELLOW+" ["+Fore.RED+"~"+Fore.YELLOW+"] Обновлений нет!")
+        print(Fore.WHITE+'', Style.BRIGHT)
+        tsu = input(' [Нажмите Enter чтобы продолжить]')
+        os.system('clear')
+    
+    else:
+        os.system('rm -fr /data/data/com.termux/files/home/Installer_Files/trash/updatepak')
+        baner()
+        print (Fore.GREEN+" ["+Fore.CYAN+"!"+Fore.GREEN+"] Есть обновление!")
+        print(Fore.WHITE+'', Style.BRIGHT)
+        print(Style.BRIGHT,Fore.YELLOW+"["+Fore.RED+"~"+Fore.YELLOW+"] Обновить Installer?")
+        print(Style.RESET_ALL)
+        uodate = input(' Выбери пункт [y/n] ➤ ')
+        
+        if uodate == 'y':
+            os.system('rm -fr /data/data/com.termux/files/home/setup_installer.py')
+            os.chdir('/data/data/com.termux/files/home/installer')
+            os.system('mv setup_installer.py /data/data/com.termux/files/home/')
+            os.system("""sed -i '/^cd && cd installer && python testinstaller.py$/d' ~/.bashrc""")
+            os.system('echo "cd && python setup_installer.py" >> ~/.bashrc')
+            print(f'\33]0; Создайте новый сезон!\a',
+                    end='', flush=True)  
+            while True:
+                os.system('clear')
+                baner()
+                print(Style.BRIGHT, Fore.YELLOW+"["+Fore.CYAN+"i"+Fore.YELLOW+"] Перезапустите Termux или создайте новый сезон!")
+                lol = input('')
+                
+        else:
+            os.system('clear')
+
+
+
+
 #доступ к файлам
 
 os.system('clear')
