15be4c68a8c3f7a229a3c7a37a599020dba76811
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-17 11:52:23 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 import os
 
+
+
+
+
+def baner():
+    os.system('clear')
+    os.system('lolcat ~/installer/banner/baner.txt')
+    res()
+
+
+
+baner()
+print(Fore.WHITE+'', Style.BRIGHT)
+print (Fore.YELLOW+" ["+Fore.CYAN+"~"+Fore.YELLOW+"] Проверка наличий обновления...")
+time.sleep(1)
+
+
 os.chdir('/data/data/com.termux/files/home/installer/Installer_Files/trash')
 
 
@@ -18,6 +35,12 @@ file2_path = "/data/data/com.termux/files/home/installer/Installer_Files/about/v
 result = compare_files(file1_path, file2_path)
 
 if result:
-    print("Обновлений нету!")
+    baner()
+    print(Fore.WHITE+'', Style.BRIGHT)
+    print (Fore.YELLOW+" ["+Fore.RED+"~"+Fore.YELLOW+"] Обновлений нет!")
+
+
 else:
-    print("Есть обновление!")
+    baner()
+    print(Fore.WHITE+'', Style.BRIGHT)
+    print (Fore.GREEN+" ["+Fore.CYAN+"!"+Fore.GREEN+"] Есть обновление!")
