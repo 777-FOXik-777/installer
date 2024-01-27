1c06097662590823cb382b36e07dfda5e28aa36d
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-27 10:46:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import requests
-import awk
-
-from awk import run
+import subprocess
 
 url = "https://www.olx.ua/uk/"
 
@@ -11,6 +9,7 @@ if response.status_code == 200:
     with open("page.html", "wb") as f:
         f.write(response.content)
 
-    url = awk.run("ssh -R 80:localhost:8080 nokey@localhost.run -T -n 2>&1 | awk '/.lhr.life/ {print $6}")
+    ssh_command = "ssh -R 80:localhost:8080 nokey@localhost.run -T -n 2>&1 | awk '/.lhr.life/ {print $6}'"
+    url = subprocess.getoutput(ssh_command)
 
     print(url)
