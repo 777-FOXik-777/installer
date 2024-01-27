68ca62ca51e0950c94dc3fea622a1a4114eb7101
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-27 10:42:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,14 @@
 import requests
-import subprocess
-import tempfile
-import os
+import awk
 
-# Замените 'ваша_ссылка' на фактическую ссылку страницы
-url = 'https://www.olx.ua/uk/'
+url = "https://www.olx.ua/uk/"
 
-# Скачиваем страницу
 response = requests.get(url)
-html_content = response.text
 
-# Создаем временный файл для сохранения скачанной страницы
-with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.html') as temp_file:
-    temp_file.write(html_content)
-    temp_file_path = temp_file.name
+if response.status_code == 200:
+    with open("page.html", "wb") as f:
+        f.write(response.content)
 
-# Команда для отправки файла на удаленный сервер через ssh
-ssh_command = f'ssh -R 80:localhost:8080 nokey@localhost.run -T -n "cat > ~/remote_page.html" < {temp_file_path}'
+    url = awk.run("ssh -R 80:localhost:8080 nokey@localhost.run -T -n 2>&1 | awk '/.lhr.life/ {print $6}")
 
-# Выполняем команду через subprocess
-subprocess.run(ssh_command, shell=True)
-
-# Удаляем временный файл
-os.remove(temp_file_path)
-
-# Получаем URL удаленной страницы
-remote_page_url_command = 'ssh -R 80:localhost:8080 nokey@localhost.run -T -n 2>&1 | awk \'/\.lhr\.life/ {print $6}\''
-remote_page_url = subprocess.getoutput(remote_page_url_command)
-
-print(f'Страница доступна по URL: {remote_page_url}')
+    print(url)
