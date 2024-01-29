aaec1054cd8485da4d23614224a51e33cc13584b
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-29 12:55:52 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -796,8 +796,10 @@ while True:
 
     
     if inp == 't':
-        os.system('xdg-open https://t.me/SYPEXHACK')
-        os.system('clear')
+        baner()
+        print(Fore.WHITE+'', Style.BRIGHT)
+        print(Fore.YELLOW+" ETH"+Fore.CYAN+" 0xf6880812E489d0dD2537Cc0244c52888F4868815")
+        exit()
 
     
     
