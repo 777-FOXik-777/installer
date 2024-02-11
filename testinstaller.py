e19ccb2f17d4ded39e5a93225cde55d8867416e4
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-02-11 19:47:31 +0200

    Update testinstaller.py

--- testinstaller.py
+++ testinstaller.py
@@ -144,8 +144,8 @@ else:
     os.system('pkg install python3 -y')
 
     #запуск на installer
-    os.system("""alias installer="cd ~/installer && python testinstaller.py" """)
-    os.system("""sed -i '/alias installer="cd ~\/installer && python testinstaller.py/d' ~/.bashrc""")
+    os.system("""alias installer="cd ~/installer && python installer.py" """)
+    os.system("""sed -i '/alias installer="cd ~\/installer && python installer.py/d' ~/.bashrc""")
     os.system("""sed -i '/printf "\\33\]0; Telegram > @SYPEXHACK\\a"/d' ~/.bashrc""")
 
   
