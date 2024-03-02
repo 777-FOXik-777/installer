a94bcba02b3f530763edb5857117b6e6d085abf6
SYPEXHACK <95873460+777-FOXik-777@users.noreply.github.com>
2024-03-02 12:24:31 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -229,16 +229,15 @@ else:
   
     os.chdir('/data/data/com.termux/files/home/Installer_Files/trash')
 
-    os.system('wget https://raw.githubusercontent.com/777-FOXik-777/installer/main/Installer_Files/trash/Auto 2>&1 | awk "sypexhack {print $1}"')
-
+  
     os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/lochost")
     os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/serveo")
     os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/IP")
     os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/holehe")
     os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/exiftool")
-
+  
+    os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/Auto")
     os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/sypexhack")
-    os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/hack")
     os.mkdir("/data/data/com.termux/files/home/Installer_Files/trash/update")
     
     print (Style.BRIGHT, Fore.GREEN+"["+Fore.CYAN+"!"+Fore.GREEN+"] Файлы УСПЕШНО установлены!")
